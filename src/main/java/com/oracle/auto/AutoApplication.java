3ab2317240a2f413aa40767e8ad19c25bc6164be
ponymano1 <zack7825@outlook.com>
2024-03-20 17:44:28 +0800

    for testing

--- src/main/java/com/oracle/auto/AutoApplication.java
+++ src/main/java/com/oracle/auto/AutoApplication.java
@@ -28,7 +28,7 @@ public class AutoApplication {
     //add log
 
     private static final String NODE_URL = "http://127.0.0.1:8545";
-    private static final String PRIVATE_KEY = "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d";
+    private static final String PRIVATE_KEY = "";
 
     private static final String CONTRACT_ADDRESS1 = "0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab";
     private static final String FUNC_NAME1 = "claimETHYield";
